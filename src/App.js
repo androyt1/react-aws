ca9cafd9e9689e66254641d2e642063c6b200cec
Andrew Aghoghovwia <androyt1@gmail.com>
2022-03-26 06:08:43 +0000

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Trying out React on AWS</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
